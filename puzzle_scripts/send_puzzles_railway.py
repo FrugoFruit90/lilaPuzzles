9dae40a3c5de38f31c0eaa824c7a59fb06516a18
Jan Zyśko <janzysko@gmail.com>
2024-02-16 18:02:53 +0100

    puzzle scripts

---
+++ puzzle_scripts/send_puzzles_railway.py
@@ -0,0 +1,43 @@
+from ast import literal_eval
+import pymongo
+
+
+def _dict(o: object) -> dict:
+    return o.__dict__ if hasattr(o, "__dict__") else o
+
+
+def _inupsert(o: object) -> object:
+    # if env.args.drop or env.args.drop_db:
+    #     return pymongo.InsertOne(o)
+    # else:
+    #     return pymongo.UpdateOne({"_id": o["_id"]}, {"$set": o}, upsert=True)
+    return pymongo.UpdateOne({"_id": o["_id"]}, {"$set": o}, upsert=True)
+
+
+def bulk_write(
+    coll: pymongo.collection.Collection,
+    objs: list,
+    append: bool = False,
+) -> None:
+    # append parameter is for bson/json export to forum collections
+    if len(objs) < 1:
+        return
+    # database mode
+    ledger = []
+    for o in objs:
+        ledger.append(_inupsert(_dict(o)))
+    res = coll.bulk_write(ledger).bulk_api_result
+
+
+uri = URI
+
+
+with open('../../../lichess-puzzler/puzzles_filtered.pgn') as f:
+    puzzles = [literal_eval(line) for line in f]
+
+client = pymongo.MongoClient(uri, authSource="admin")
+db = client.get_default_database()
+
+bulk_write(db.puzzle2_puzzle, puzzles)
+print(db.puzzle2_puzzle)
+client.close()
