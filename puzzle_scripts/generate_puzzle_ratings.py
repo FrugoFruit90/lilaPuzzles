52e996e37ef2b37287d05a61e4c8f7c677e2a5ec
Jan Zyśko <janzysko@gmail.com>
2025-01-23 18:47:15 +0100

    save progress

--- puzzle_scripts/generate_puzzle_ratings.py
+++ puzzle_scripts/generate_puzzle_ratings.py
@@ -1,8 +1,9 @@
 import datetime
 
 import bson
-import glicko2
 import json
+
+import glicko2
 import lichess.api
 import pandas as pd
 
@@ -11,17 +12,18 @@ def get_all_user_puzzle_ratings(users: list):
     user_ratings = {}
     for user in users:
         try:
-            user_data = lichess.api.user(user["_id"])
+            user_data = lichess.api.user(user)
             if user_data.get("disabled", None):
-                print(f"User {user['_id']} is banned!")
+                print(f"User {user} is banned!")
                 continue
             rating_dict = user_data["perfs"].get("puzzle", {"games": 0})
-            if rating_dict["games"] < 25:
-                print(f"User {user['_id']} doesn't have enough games")
+            if rating_dict["games"] < 1:
+                print(f"User {user} doesn't have enough games")
+                user_ratings[user] = rating_dict
             else:
-                user_ratings[user["_id"]] = rating_dict
+                user_ratings[user] = rating_dict
         except lichess.api.ApiHttpError:
-            print(f"User {user['_id']} doesn't have a lichess account!")
+            print(f"User {user} doesn't have a lichess account!")
     frugo_data = lichess.api.user("FrugoFruit90")
     user_ratings["frugofruit90"] = frugo_data["perfs"]["puzzle"]
 
@@ -29,9 +31,11 @@ def get_all_user_puzzle_ratings(users: list):
 
 
 if __name__ == "__main__":
-    DUMP_FOLDER_NAME = "dump_08_19"
-    RATING_FOLDER_NAME = "user_ratings_08_19"
-    UPDATE_USER_RATINGS = True
+    DUMP_FOLDER_NAME = "dump_09_01"
+    USER_RATINGS_OLD = "08_13"
+    UPDATE_USER_RATINGS = False
+
+    replace_dict = {"fjd": "f-dunne", "fruggio": "frugofruit90", "mszczepaniak": "Szczepaniak"}
 
     with open(f'data/{DUMP_FOLDER_NAME}/lichess/puzzle2_round.bson', 'rb') as f:
         puzzle_attempts = pd.DataFrame(bson.decode_all(f.read()))
@@ -43,20 +47,25 @@ if __name__ == "__main__":
         puzzle_attempts_fruggio['u'] = "frugofruit90"
 
     puzzle_attempts = pd.concat([puzzle_attempts_fruggio, puzzle_attempts], axis=0)
+    puzzle_attempts["u"] = puzzle_attempts["u"].replace(replace_dict)
 
-    with open(f'data/{DUMP_FOLDER_NAME}/lichess/user4.bson', 'rb') as f:
-        users = bson.decode_all(f.read())
+    users = puzzle_attempts["u"].unique()
+
+    user_ratings_past = json.load(open(f'data/user_ratings_{USER_RATINGS_OLD}.json'))
+    user_ratings_past.update(json.load(open(f'data/user_ratings_2024-09-01.json')))
     if UPDATE_USER_RATINGS:
-        user_ratings = get_all_user_puzzle_ratings(users)
-        json.dump(user_ratings, open(RATING_FOLDER_NAME, 'w'))
+        user_ratings_new = get_all_user_puzzle_ratings(users)
+        json.dump(user_ratings_new, open(f'data/user_ratings_{datetime.date.today()}.json', 'w'))
     else:
-        user_ratings = json.load(open(RATING_FOLDER_NAME))
-
-    for user in user_ratings:
-        user_ratings[user]["rating"] -= 200
+        user_ratings_new = {}
+    user_ratings_past.update(user_ratings_new)
+    user_ratings = user_ratings_past
 
     user_ratings_df = pd.DataFrame.from_dict(user_ratings, orient="index").rename(
         columns={"rating": "player_rating", "rd": "player_rd"})
+    user_ratings_df["player_rating"] -= 200
+    user_ratings_df = user_ratings_df.fillna({"player_rating": 1500, "player_rd": 500})
+
     puzzle_attempts = puzzle_attempts.merge(user_ratings_df, right_index=True, left_on="u")
     puzzle_ratings_final = {}
     puzzle_ratings_leaderboard = {}
@@ -78,15 +87,17 @@ if __name__ == "__main__":
             "no_successes": int(puzzle_df["w"].sum())
         }
         puzzle_ratings_final[puzzle_id] = puzzle_rating
-        print(sum([puzzle["no_tries"] for puzzle in puzzle_ratings_final.values()]))
-        print(sum([puzzle["no_tries"] > 15 for puzzle in puzzle_ratings_final.values()]))
-        print(sum([puzzle["rd"] < 130 for puzzle in puzzle_ratings_final.values()]))
-        print(sorted([puzzle["no_tries"] for puzzle in puzzle_ratings_final.values()]))
+        puzzle_ratings_leaderboard[puzzle_id] = puzzle_rating
+        puzzle_evaluation[puzzle_id] = puzzle_rating["rating"]
 
-        if puzzle_rating["rd"] < 130:
-            puzzle_ratings_leaderboard[puzzle_id] = puzzle_rating
-            puzzle_evaluation[puzzle_id] = puzzle_rating["rating"]
-
+    puzzle_ratings_final
+    puzzle_df_test = puzzle_attempts[puzzle_attempts["puzzle_id"] == 'p0063']
+    puzzle_df_test[["player_rating", "player_rd", "w"]]
+#     print(sum([puzzle["no_tries"] for puzzle in puzzle_ratings_final.values()]))
+#     print(sum([puzzle["no_tries"] > 15 for puzzle in puzzle_ratings_final.values()]))
+#     print(sum([puzzle["rd"] < 130 for puzzle in puzzle_ratings_final.values()]))
+#     print(sorted([puzzle["no_tries"] for puzzle in puzzle_ratings_final.values()]))
+#
     json.dump(
         puzzle_ratings_final,
         open(f"data/puzzle_ratings_final_{datetime.date.today()}.json", 'w'),
@@ -107,7 +118,7 @@ if __name__ == "__main__":
 
 puzzle_evaluation_df = pd.DataFrame.from_dict(puzzle_evaluation, orient="index")
 
-full_eval = pd.read_csv("puzzle_scripts/test_data.csv").reset_index()
+full_eval = pd.read_csv("data/test_data.csv").reset_index()
 full_eval_merged = full_eval.merge(puzzle_evaluation_df, left_on="PuzzleId", right_index=True)
 full_eval_merged.loc[:, 0] = full_eval_merged.loc[:, 0].round().astype(int)
 full_eval_merged.loc[:, 0].to_dict()
@@ -117,3 +128,5 @@ json.dump(
     open(f"data/eval_final_{datetime.date.today()}.json", 'w'),
     indent=4
 )
+
+full_eval_merged[["PuzzleId", "0"]].to_csv("final_data.csv", index=False)
--- puzzle_scripts/lottery_winners.py
+++ puzzle_scripts/lottery_winners.py
@@ -7,7 +7,7 @@ import pandas as pd
 # os.getcwd()
 # os.chdir("../..")
 
-DUMP_FOLDER_NAME = "dump_08_26"
+DUMP_FOLDER_NAME = "dump_31_08"
 
 with open(f'data/{DUMP_FOLDER_NAME}/lichess/puzzle2_round.bson', 'rb') as f:
     puzzle_attempts = pd.DataFrame(bson.decode_all(f.read()))
@@ -15,8 +15,8 @@ with open(f'data/{DUMP_FOLDER_NAME}/lichess/puzzle2_round.bson', 'rb') as f:
 
 puzzle_attempts["ones"] = 1
 print(puzzle_attempts.columns)
-user_counts = puzzle_attempts[puzzle_attempts["d"] > "2024-08-19 00:00:00.000"].groupby("u")["ones"].count()
-print(user_counts[user_counts >= 100])
-gods = user_counts[user_counts >= 100].index.to_list()
+user_counts = puzzle_attempts[puzzle_attempts["d"] > "2024-08-11 00:00:00.000"].groupby("u")["ones"].count()
+print(user_counts[user_counts >= 300])
+gods = user_counts[user_counts >= 300].index.to_list()
 print(gods)
 print(random.sample(gods, k=3))
